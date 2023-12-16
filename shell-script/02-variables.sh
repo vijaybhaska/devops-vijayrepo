48e498588f793c3a83abe3fa29c57bfa40a97792
vijaybhaska <baskar.vijay49@gmail.com>
2023-12-16 20:55:05 +0530

    adding 2nd script

--- shell-script/02-variables.sh
+++ shell-script/02-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
- $PERSON1=Ramesh
- $PERSON2=Suresh
- $PERSON3=Raheem
+ PERSON1=Ramesh
+ PERSON2=Suresh
+ PERSON3=Raheem
 echo "$PERSON2: Hello $PERSON1 good morning"
 echo "$PERSON1: $PERSON2 am learning devops"
 echo "$PERSON3: Hi $PERSON1 am to learing devops"
